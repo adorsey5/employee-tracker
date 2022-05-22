026fe764406854b562a2fe80b79cb16bab0dd4ec
A. Dorsey <ashadorsey@gmail.com>
2022-05-22 14:11:46 -0400

    added sql files

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT,
+   CONSTRAINT fk_department 
+   FOREIGN KEY (department_id) 
+   REFERENCES departments(id)
+   ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT REFERENCES employees(id), 
+    CONSTRAINT fk_role
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+);
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO departments (name)
+VALUES
+('Marketing'),
+('Sales'),
+('Finance'),
+('Management');
+('Engineering');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Marketing Director', 80000.00, 1),
+('Salesperson', 48000.00, 2),
+('Accountant', 60000.00, 3),
+('Manager', 75000.00, 4),
+('Engineer', 85000.00, 5),
+('Branding Consultant', 70000.00, 1),
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Jane', 'Doe', 1, 2),
+('John', 'Smith', 2, 3),
+('Harry', 'Potter', 4, 5),
+('Ebony', 'Ivory', 5, 6),
+('Lionel', 'Linen', 5, 7),
+('Joe', 'Jackson', 6, 8);
